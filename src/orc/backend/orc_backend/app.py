14bced94d4c71c14e3357124a2cadd7183b0eb64
John Culbert <john.culbert@gesis.org>
2024-08-22 12:27:48 +0000

    update app to allow CORS Requests

--- src/orc/backend/orc_backend/app.py
+++ src/orc/backend/orc_backend/app.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
-from flask import Flask, request
+from flask import CORS, Flask, cross_origin, request
 from orc.backend.orc_backend.open_research_converter import OpenResearchConverter
 
 app = Flask(__name__)
 orc = OpenResearchConverter()
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
 @app.route("/", methods=["GET"])
+@cross_origin()
 def hello_world():
     description = """
                 <!DOCTYPE html>
@@ -23,25 +26,30 @@ def hello_world():
 
 
 @app.route("/healthcheck", methods=["GET"])
+@cross_origin()
 async def healthcheck():
     return await orc.health_check()
 
 
 @app.route("/start_processing", methods=["POST"])
+@cross_origin()
 async def start_processing():
+    json_data = request.get_json()
     job_id = orc.generate_new_job()
-    text = request.form["input_data"]
-    email = request.form["email"]
+    text = json_data["input_data"]
+    email = json_data["email"]
     return await orc.process(job_id, text, email)
 
 
 @app.route("/get_status", methods=["POST"])
+@cross_origin()
 def get_status():
     job_id = request.form["job_id"]
     return orc.get_status(job_id)
 
 
 @app.route("/get_data", methods=["GET"])
+@cross_origin()
 def send_data():
     job_id = request.form["job_id"]
     return orc.return_data(job_id)
