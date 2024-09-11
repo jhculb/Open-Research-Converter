6fb728a8b87d7aa565c3b2c1c0d7c3ca80c69017
John Culbert <john.culbert@gesis.org>
2024-09-11 10:44:14 +0000

    better logging strings, data validation / processing and removing of taskgroup

--- src/orc/backend/orc_backend/requester.py
+++ src/orc/backend/orc_backend/requester.py
@@ -79,14 +79,15 @@ class openalex_requester:
         chunked_data = self._chunk_input_data(job_id)
         if chunked_data is not None:
             chunked_data = list(chunked_data)
-            async with asyncio.TaskGroup() as tg:
-                for pos, (chunks, chunklen) in enumerate(chunked_data):
-                    self._jobs[job_id]["_tasklist"].append(tg.create_task(self._request(chunks, job_id, chunklen, pos)))
-                    self._logger.debug(f"Request task added for chunk {pos} of {job_id}")
+            tasks = set()
+            for pos, (chunks, chunklen) in enumerate(chunked_data):
+                tasks.add(asyncio.create_task(self._request(chunks, job_id, chunklen, pos)))
+                self._logger.debug(f"job_id: {job_id}: Request task added for chunk {pos} of {job_id}")
+            await asyncio.gather(*tasks)
             results = [self._jobs[job_id]["responses"][pos_iter] for pos_iter in range(0, len(chunked_data))]
-            self._logger.info(f"results completed for {job_id}")
+            self._logger.info(f"job_id: {job_id}: results completed for {job_id}")
         else:
-            self._logger.error(f"Chunking failed in process for {job_id}, returning False")
+            self._logger.error(f"job_id: {job_id}: Chunking failed in process for {job_id}, returning False")
         output = list(itertools.chain.from_iterable(results))
         self._jobs[job_id]["output_data"] = output
         self._jobs[job_id]["status"] = "complete"
@@ -95,7 +96,7 @@ class openalex_requester:
         self, job_id: str, chunksize: int = 50
     ) -> Generator[tuple[list[str], int], None, None] | None:
         if not isinstance(chunksize, int):
-            self._logger.error("Non-int passed as chunk")
+            self._logger.error(f"job_id: {job_id}: Non-int passed as chunk")
             return None
         if 0 < chunksize and chunksize < 51:
             for i in range(0, len(self._jobs[job_id]["input_data"]), chunksize):
@@ -104,19 +105,26 @@ class openalex_requester:
                     len(self._jobs[job_id]["input_data"][i : i + chunksize]),
                 )
         else:
-            self._logger.error("Chunksize parameter was outside range [1,50]")
+            self._logger.error(f"job_id: {job_id}: Chunksize parameter was outside range [1,50]")
             return None
 
     async def _request(self, chunked_data: list[str], job_id: str, chunklen: int, pos: int) -> None:
-        self._logger.info(f"sending request {pos} for job {job_id}")
+        self._logger.info(f"job_id: {job_id}: sending request {pos}")
         formatted_chunk = "|".join(chunked_data)
+        self._logger.debug(f"job_id: {job_id}: formatted_chunk: {formatted_chunk}")
+        self._logger.debug(f"job_id: {job_id}: chunklen: {chunklen}")
         response = await self._client.get(
             f'https://api.openalex.org/works?filter=doi:{formatted_chunk}&per-page={chunklen}&mailto={self._jobs[job_id]["email"]}&select=id,doi'
         )
+        self._logger.debug(f"response: {response}")
         # TODO: add Nina Request functionality for searching the title && Year && Surname / All authors
         shuffled_ids = [work["id"] for work in response.json()["results"]]
+        self._logger.debug(f"job_id: {job_id}: shuffled_ids: {shuffled_ids}")
         dois = [work["doi"] for work in response.json()["results"]]
+        self._logger.debug(f"job_id: {job_id}: dois: {dois}")
+        self._logger.debug(f"job_id: {job_id}: chunked_data: {chunked_data}")
         positions = [list(map(str.lower, chunked_data)).index(doi) for doi in dois]
         ids = [x for _, x in sorted(zip(positions, shuffled_ids, strict=True), key=lambda pair: pair[0])]
         self._jobs[job_id]["responses"][pos] = ids
         self._jobs[job_id]["progress"] += chunklen
+        self._logger.info(f"job_id: {job_id}: _request {pos} complete")
