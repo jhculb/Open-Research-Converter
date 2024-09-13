de5721231b19292a61763adc33c777235edabd07
Muhammad Ahsan Shahid <ahsan.shahid@gesis.org>
2024-09-13 12:08:41 +0200

    added hint for input file template and adjusted header style

--- src/orc/frontend/orc-demo/src/components/Header.jsx
+++ src/orc/frontend/orc-demo/src/components/Header.jsx
@@ -1,23 +1,19 @@
 import {Component} from "react";
-import './styles/Header.sass'
-import orc_logo from '../images/orc-logo.png'
-import gesis_logo from '../images/logo_gesis.svg'
+import './styles/Header.sass';
+import orc_logo from '../images/orc-logo.png';
+import gesis_logo from '../images/logo_gesis.svg';
 
 class Header extends Component {
     render() {
         return <>
-            <div className="col-2">
-                <img className="mt-2 justify-content-center img-header" src={gesis_logo} alt="gesis" onClick={()=>window.open(this.props.href, "_self")}/>
+            <div className="col-12 col-md-2 d-flex justify-content-center justify-content-md-start">
+                <img className="mt-2 img-fluid img-header" src={gesis_logo} alt="gesis" onClick={()=>window.open(this.props.href, "_self")}/>
             </div>
-            <h1 className="col-10 mt-2 d-flex justify-content-center title-color">
-                {this.props.title}
-            </h1>
-            {/*<h1 className="col-12 d-flex justify-content-center" onClick={()=>window.open(this.props.href, "_self")}>*/}
-            {/*    {this.props.title}*/}
-            {/*</h1>*/}
-            {/*<div className="col-12 d-flex justify-content-center">*/}
-            {/*    <img className='img-header' src={orc_logo} alt="orc" onClick={()=>window.open(this.props.href, "_self")}/>*/}
-            {/*</div>*/}
+            <div className="col-12 col-md-10 mt-2 d-flex justify-content-center justify-content-md-center">
+                <h1 className="title-color text-center mr-md-100">
+                    {this.props.title}
+                </h1>
+            </div>
         </>;
     }
 }
